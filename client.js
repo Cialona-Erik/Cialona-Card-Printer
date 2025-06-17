80dfa7a636f50c607d3211b55ab6bda13f032388
Cialona-Erik <Erik@cialona.com>
2025-06-17 11:37:11 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,30 +1,19 @@
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, opts) {
+  'card-buttons': function(t) {
     return [{
-      icon: 'https://cdn-icons-png.flaticon.com/512/1159/1159633.png',
+      icon: 'https://github.com/Cialona-Erik/Trello-Pretty-Card-Printer/blob/main/printer.png?raw=true',
       text: 'Print Card',
       callback: function(t) {
-        return t.card('name', 'desc', 'labels', 'checklists')
-          .then(card => {
-            const url = `./print.html?name=${encodeURIComponent(card.name)}&desc=${encodeURIComponent(card.desc)}&labels=${encodeURIComponent(JSON.stringify(card.labels))}`;
-            return t.modal({
-              url: url,
-              fullscreen: false,
-              height: 500,
-              title: 'Print Card'
-            });
+        return t.card('name', 'desc', 'labels', 'checklists').then(function(card) {
+          const url = `./print.html?name=${encodeURIComponent(card.name)}&desc=${encodeURIComponent(card.desc)}&labels=${encodeURIComponent(JSON.stringify(card.labels))}`;
+          return t.modal({
+            url: url,
+            fullscreen: false,
+            height: 500,
+            title: 'Print Card'
           });
+        });
       }
     }];
   }
 });
-
-// Expose for index.html button, too
-window.printCard = function() {
-  const t = TrelloPowerUp.iframe();
-  t.card('name', 'desc', 'labels', 'checklists')
-    .then(card => {
-      const url = `./print.html?name=${encodeURIComponent(card.name)}&desc=${encodeURIComponent(card.desc)}&labels=${encodeURIComponent(JSON.stringify(card.labels))}`;
-      t.modal({ url, fullscreen: false, height: 500, title: 'Print Card' });
-    });
-};
