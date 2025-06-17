863e2a9f736882641c34aefa2fad2ac6d0d5b279
Cialona-Erik <Erik@cialona.com>
2025-06-17 12:59:25 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 TrelloPowerUp.initialize({
   'card-buttons': function(t) {
     return [{
-      icon: 'https://github.com/Cialona-Erik/Trello-Pretty-Card-Printer/blob/main/printer.png?raw=true',
+      icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer.png',
       text: 'Print Card',
       callback: function(t) {
         return t.card('name', 'desc', 'labels', 'checklists').then(function(card) {
