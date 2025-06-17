c0aa6554a03c101f82a2533df31ee2ca18d271a0
Cialona-Erik <Erik@cialona.com>
2025-06-17 13:13:46 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,11 +1,28 @@
+// Load Trello Power-Up functionality
+const t = TrelloPowerUp.iframe();
+
+// This function is used when the iframe (index.html) has a button that calls window.printCard()
+window.printCard = function () {
+  return t.card('name', 'desc', 'labels', 'checklists').then(function (card) {
+    const url = `https://cialona-erik.github.io/Cialona-Card-Printer/print.html?name=${encodeURIComponent(card.name)}&desc=${encodeURIComponent(card.desc)}&labels=${encodeURIComponent(JSON.stringify(card.labels))}`;
+    return t.modal({
+      url: url,
+      fullscreen: false,
+      height: 500,
+      title: 'Print Card'
+    });
+  });
+};
+
+// Register the Power-Up capabilities — this is required!
 TrelloPowerUp.initialize({
-  'card-buttons': function(t) {
+  'card-buttons': function (t) {
     return [{
       icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer.png',
       text: 'Print Card',
-      callback: function(t) {
-        return t.card('name', 'desc', 'labels', 'checklists').then(function(card) {
-          const url = `./print.html?name=${encodeURIComponent(card.name)}&desc=${encodeURIComponent(card.desc)}&labels=${encodeURIComponent(JSON.stringify(card.labels))}`;
+      callback: function (t) {
+        return t.card('name', 'desc', 'labels', 'checklists').then(function (card) {
+          const url = `https://cialona-erik.github.io/Cialona-Card-Printer/print.html?name=${encodeURIComponent(card.name)}&desc=${encodeURIComponent(card.desc)}&labels=${encodeURIComponent(JSON.stringify(card.labels))}`;
           return t.modal({
             url: url,
             fullscreen: false,
@@ -15,5 +32,16 @@ TrelloPowerUp.initialize({
         });
       }
     }];
+  },
+  'card-back-section': function (t) {
+    return {
+      title: 'Card Tools',
+      icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer.png',
+      content: {
+        type: 'iframe',
+        url: 'https://cialona-erik.github.io/Cialona-Card-Printer/index.html',
+        height: 50
+      }
+    };
   }
 });
