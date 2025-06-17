143b1ddbe770dd54997e7328da1947fc907c8e8f
Cialona-Erik <Erik@cialona.com>
2025-06-17 15:58:07 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
 TrelloPowerUp.initialize({
   'card-buttons': function(t) {
     return [{
-      icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer-small.png',
+      icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer-small.png', // Consider using SVG for better theming
       text: 'Print Card',
+      condition: 'edit', // Show button only to users who can edit the board
       callback: function(t) {
         return t.card('name', 'desc').then(card => {
           const url = `https://cialona-erik.github.io/Cialona-Card-Printer/print.html?name=${encodeURIComponent(card.name)}&desc=${encodeURIComponent(card.desc)}`;
