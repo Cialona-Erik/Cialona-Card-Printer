5ce2b40a2063b1f65a606abf8fddbafe13e41e9c
Cialona-Erik <Erik@cialona.com>
2025-06-17 15:25:22 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,41 +1,25 @@
 TrelloPowerUp.initialize({
-  'card-buttons': function (t) {
+  'card-buttons': function(t) {
     return [{
-      icon: 'https://super-moxie-650636.netlify.app/printer-small.png',
+      icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer-small.png',
       text: 'Print Card',
-      callback: function (t) {
-        return t.card('name', 'desc', 'labels', 'checklists').then(function (card) {
-          const url = `https://super-moxie-650636.netlify.app/print.html?name=${encodeURIComponent(card.name)}&desc=${encodeURIComponent(card.desc)}&labels=${encodeURIComponent(JSON.stringify(card.labels))}`;
-          return t.modal({
-            url: url,
-            fullscreen: false,
-            height: 500,
-            title: 'Print Card'
-          });
+      callback: function(t) {
+        return t.card('name', 'desc').then(card => {
+          const url = `https://cialona-erik.github.io/Cialona-Card-Printer/print.html?name=${encodeURIComponent(card.name)}&desc=${encodeURIComponent(card.desc)}`;
+          return t.modal({ url, title: 'Print Card', height: 500 });
         });
       }
     }];
   },
-
-  'card-back-section': function (t) {
+  'card-back-section': function(t) {
     return {
       title: 'Card Tools',
-      icon: 'https://super-moxie-650636.netlify.app/printer-small.png',
+      icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer-small.png',
       content: {
         type: 'iframe',
-        url: 'https://super-moxie-650636.netlify.app/index.html',
+        url: 'https://cialona-erik.github.io/Cialona-Card-Printer/index.html',
         height: 50
       }
     };
   }
 });
-
-window.printCard = function () {
-  return TrelloPowerUp.iframe().then(t => {
-    return t.card('name', 'desc', 'labels', 'checklists')
-      .then(function (card) {
-        const url = `https://super-moxie-650636.netlify.app/print.html?name=${encodeURIComponent(card.name)}&desc=${encodeURIComponent(card.desc)}&labels=${encodeURIComponent(JSON.stringify(card.labels))}`;
-        return t.modal({ url, fullscreen: false, height: 500, title: 'Print Card' });
-      });
-  });
-};
