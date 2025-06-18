fe5157da0e461152a09a1114720474bf100932e7
Cialona-Erik <Erik@cialona.com>
2025-06-18 09:12:42 +0200

    Update client.js

--- client.js
+++ client.js
@@ -2,50 +2,44 @@ console.log('client.js loaded - TrelloPowerUp initialize starting');
 
 TrelloPowerUp.initialize({
   'card-buttons': function(t) {
+    console.log('card-buttons handler called');
     return [{
       icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer-small.png',
       text: 'Print Card',
       condition: 'edit',
       callback: function(t) {
-        // Only allowed fields!
-        return t.card('id', 'name', 'desc', 'due', 'idMembers', 'cover')
-        .then(function(card) {
-          // Get all board members
-          return t.members('id', 'fullName', 'avatarUrl').then(function(members) {
-            // Match card's member IDs to member objects
-            const memberData = (card.idMembers || []).map(id =>
-              members.find(m => m.id === id)
-            ).filter(Boolean);
+        console.log('Print Card button clicked');
+        // Get name, desc, and cover from card
+        return t.card('name', 'desc', 'cover').then(card => {
+          console.log('Card data fetched:', card);
 
-            // Get the actual cover image URL (if any)
-            let coverUrl = '';
-            if (card.cover && card.cover.sharedSourceUrl) {
-              coverUrl = card.cover.sharedSourceUrl;
-            } else if (card.cover && card.cover.url) {
-              coverUrl = card.cover.url;
-            }
+          // Get the actual cover image URL (if any)
+          let coverUrl = '';
+          if (card.cover && card.cover.sharedSourceUrl) {
+            coverUrl = card.cover.sharedSourceUrl;
+          } else if (card.cover && card.cover.url) {
+            coverUrl = card.cover.url;
+          }
 
-            // Compose URL with due date and members as JSON
-            const url =
-              'https://cialona-erik.github.io/Cialona-Card-Printer/print.html?' +
-              'name=' + encodeURIComponent(card.name) +
-              '&desc=' + encodeURIComponent(card.desc) +
-              (coverUrl ? '&cover=' + encodeURIComponent(coverUrl) : '') +
-              (card.due ? '&due=' + encodeURIComponent(card.due) : '') +
-              (memberData.length ? '&members=' + encodeURIComponent(JSON.stringify(memberData)) : '');
+          const url =
+            'https://cialona-erik.github.io/Cialona-Card-Printer/print.html?' +
+            'name=' + encodeURIComponent(card.name) +
+            '&desc=' + encodeURIComponent(card.desc) +
+            (coverUrl ? '&cover=' + encodeURIComponent(coverUrl) : '');
 
-            return t.modal({
-              url,
-              title: 'Print Card',
-              height: 500,
-              width: 880,
-              fullscreen: false
-            });
+          console.log('Opening modal with URL:', url);
+          return t.modal({
+            url,
+            title: 'Print Card',
+            height: 500,
+            width: 880,
+            fullscreen: false
           });
         });
       }
     }];
   },
+
   'card-back-section': function(t) {
     console.log('card-back-section handler called');
     return {
@@ -59,3 +53,4 @@ TrelloPowerUp.initialize({
     };
   }
 });
+
