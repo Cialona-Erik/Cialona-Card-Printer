7f222e46a22c51e1882504d36b22d03953943ada
Cialona-Erik <Erik@cialona.com>
2025-06-18 09:04:31 +0200

    Update client.js

--- client.js
+++ client.js
@@ -2,38 +2,38 @@ console.log('client.js loaded - TrelloPowerUp initialize starting');
 
 TrelloPowerUp.initialize({
   'card-buttons': function(t) {
-    console.log('card-buttons handler called');
     return [{
       icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer-small.png',
       text: 'Print Card',
       condition: 'edit',
       callback: function(t) {
-        console.log('Print Card button clicked');
-        // Get all needed card fields
-        return t.card('name', 'desc', 'cover', 'idMembers', 'due').then(card => {
-          console.log('Card data fetched:', card);
+        // Only allowed fields!
+        return t.card('id', 'name', 'desc', 'due', 'idMembers', 'cover')
+        .then(function(card) {
+          // Get all board members
+          return t.members('id', 'fullName', 'avatarUrl').then(function(members) {
+            // Match card's member IDs to member objects
+            const memberData = (card.idMembers || []).map(id =>
+              members.find(m => m.id === id)
+            ).filter(Boolean);
 
-          // Get the actual cover image URL (if any)
-          let coverUrl = '';
-          if (card.cover && card.cover.sharedSourceUrl) {
-            coverUrl = card.cover.sharedSourceUrl;
-          } else if (card.cover && card.cover.url) {
-            coverUrl = card.cover.url;
-          }
+            // Get the actual cover image URL (if any)
+            let coverUrl = '';
+            if (card.cover && card.cover.sharedSourceUrl) {
+              coverUrl = card.cover.sharedSourceUrl;
+            } else if (card.cover && card.cover.url) {
+              coverUrl = card.cover.url;
+            }
 
-          // Now, get the members info by IDs
-          return t.members('id', 'fullName', 'avatarUrl').then(members => {
-            // Only include members that are assigned to this card
-            const cardMembers = members.filter(m => (card.idMembers || []).includes(m.id));
+            // Compose URL with due date and members as JSON
             const url =
               'https://cialona-erik.github.io/Cialona-Card-Printer/print.html?' +
               'name=' + encodeURIComponent(card.name) +
               '&desc=' + encodeURIComponent(card.desc) +
               (coverUrl ? '&cover=' + encodeURIComponent(coverUrl) : '') +
               (card.due ? '&due=' + encodeURIComponent(card.due) : '') +
-              '&members=' + encodeURIComponent(JSON.stringify(cardMembers));
+              (memberData.length ? '&members=' + encodeURIComponent(JSON.stringify(memberData)) : '');
 
-            console.log('Opening modal with URL:', url);
             return t.modal({
               url,
               title: 'Print Card',
@@ -46,7 +46,6 @@ TrelloPowerUp.initialize({
       }
     }];
   },
-
   'card-back-section': function(t) {
     console.log('card-back-section handler called');
     return {
