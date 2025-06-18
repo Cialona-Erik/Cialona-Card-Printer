5d91700904e2095922b059872b59e31882b83dea
Cialona-Erik <Erik@cialona.com>
2025-06-18 10:43:38 +0200

    Update client.js

--- client.js
+++ client.js
@@ -54,16 +54,16 @@ TrelloPowerUp.initialize({
     }];
   },
 
-  'card-back-section': function(t) {
-    console.log('card-back-section handler called');
-    return {
-      title: 'Card Tools',
-      icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer-small.png',
-      content: {
-        type: 'iframe',
-        url: 'https://cialona-erik.github.io/Cialona-Card-Printer/index.html',
-        height: 50
-      }
-    };
-  }
+//  'card-back-section': function(t) {
+//    console.log('card-back-section handler called');
+//    return {
+//      title: 'Card Tools',
+//      icon: 'https://cialona-erik.github.io/Cialona-Card-Printer/printer-small.png',
+//      content: {
+//        type: 'iframe',
+//        url: 'https://cialona-erik.github.io/Cialona-Card-Printer/index.html',
+//        height: 50
+//      }
+//    };
+//  }
 });
